f0a72c2d26e69035429fd08831f00c0b9bb4a1e5
sudeeptha967 <53205609+sudeeptha967@users.noreply.github.com>
2019-07-23 14:57:02 +0530

    Add files via upload

---
+++ src/test/java/cucumber_testme_demo/runner.java
@@ -0,0 +1,17 @@
+package cucumber_testme_demo;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/feature/feature1.feature",glue="cucumber_testme_demo",plugin="json:target\\Jsonreport.json")
+//tags={"~@regression"}) //ignore regression
+//tags={"@login","@regression"})// - AND case
+//tags={"@login,@regression"},
+//plugin="html:target\\HtmlReport") // OR case
+//plugin="json:target\\Jsonreport.json")
+//plugin="junit:target\\xmlreport.xml")
+public class runner {
+}
---
+++ src/test/resources/feature/feature1.feature
@@ -0,0 +1,23 @@
+Feature: sign up functionality
+Scenario Outline: sigup verification
+When open home page
+And enter the username '<uname>'
+And enter the firstName
+And enter the lastname
+And enter the password
+And enter the confirm password
+And enter the gender
+And enter the email
+And enter the mobile no
+And enter	the date of birth
+And enter the address
+And enter the security question
+And enter the answer
+Then click register
+And validate the page
+Examples:
+|uname     |
+|mercurymercury|
+
+
+    
